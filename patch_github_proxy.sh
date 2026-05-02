193bf1ea41a573e6af7ed89927fa52fdde0d3cf4
HDYOU <32186506+HDYOU@users.noreply.github.com>
2026-05-02 23:42:44 +0800

    patch_github_proxy.sh

--- patch_github_proxy.sh
+++ patch_github_proxy.sh
@@ -122,7 +122,8 @@ fi
 echo "⇒ API 请求自动加代理"
 #sed -i 's/apiUrl/&.notFoundAndAppendHost((await getSourceConfigSettingValues())\[\"GHProxyPrefix\"\])/g' "$FILE"
 sed -i 's/sourceRequest[^,]+/&.notFoundAndAppendHost((await getSourceConfigSettingValues())\[\"GHProxyPrefix\"\])/g' "$FILE"
-sed -i 's/per_page=100[^,]+/&.notFoundAndAppendHost((await getSourceConfigSettingValues())\[\"GHProxyPrefix\"\])/g' "$FILE"
+sed -i '/^sourceRequest(/s/\([^,]*\),/\1.notFoundAndAppendHost((await getSourceConfigSettingValues())\[\"GHProxyPrefix\"\]),/g'  "$FILE"
+#sed -i 's/per_page=100[^,]+/&.notFoundAndAppendHost((await getSourceConfigSettingValues())\[\"GHProxyPrefix\"\])/g' "$FILE"
 
 # ==========================
 # 6. 下载代理方法
